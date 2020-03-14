7a20fe049c5bf5d627838cf410a4ee9f8793d266
砂川朝恭 <kokoronosoudanshitsu_happiness@yahoo.co.jp>
2020-03-15 08:19:15 +0900

    base for app

---
+++ app/controllers/basic_pages_controller.rb
@@ -0,0 +1,34 @@
+class BasicPagesController < ApplicationController
+    def home
+    end
+
+    def about
+    end
+
+    def contact
+    end
+
+    def recruit
+    end
+
+    def ir
+    end
+
+    def news
+    end
+
+    def service
+    end
+
+    def help
+    end
+
+    def security
+    end
+
+    def privacypolicy
+    end
+
+    # def
+    # end
+end
